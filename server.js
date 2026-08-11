c9e8fde23468cdeafdd9fee11b88e17d666c67a5
Aftab Mumtaz <aftabmumtaz14@gmail.com>
2026-08-11 14:14:45 +0500

    changes for cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL || 'https://trade-now-umber.vercel.app', credentials: true }));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
